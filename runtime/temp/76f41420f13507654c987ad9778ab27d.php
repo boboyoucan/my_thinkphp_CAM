cd0659e8f4fcd952ed2e189af0936c7e2727293d
“wangbowen_myweb <root@boboyoucan.cn>
2016-11-27 13:37:28 +0800

    最新

---
+++ runtime/temp/76f41420f13507654c987ad9778ab27d.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:54:"/mnt/www/html/myphpweb/apps/index/view/user/index.html";i:1479133251;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>你好</title>
+</head>
+<body>
+user
+</body>
+</html>
---
+++ runtime/temp/db32ff542b916786d1a6a65b3d97c411.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:64:"/mnt/www/html/myphpweb/public/../apps/index/view/user/index.html";i:1479133251;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>你好</title>
+</head>
+<body>
+user
+</body>
+</html>
